8cd2f589005cec884c3ff63bfc3d5f207c31206c
halle tempest and phat nguyen <phat@>
2017-04-25 14:22:41 -0700

    added toggle background color

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+
+  });
+});
